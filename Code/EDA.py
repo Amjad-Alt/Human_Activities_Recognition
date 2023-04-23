505987cb25f73cb366ff9bee132035d645e74816
Amjad Altuwayjiri <amjadibrahim1994@gmail.com>
2023-04-23 19:30:40 -0400

    Update EDA.py
    clean file

--- Code/EDA.py
+++ Code/EDA.py
@@ -12,64 +12,48 @@ from collections import Counter
 from sklearn.cluster import DBSCAN 
 
 
+#============================== Joining datasets =================================
 
-#----------------------LOAD DATA --------------------------
-
-#=======================Grouping test and train =================================
 # Set the working directory to the directory containing the datasets
-#os.chdir("C:/Users/amjad/OneDrive/المستندات/GWU_Cources/Spring2023/Machine_Learning/MLproject/Code")
+os.chdir("C:/Users/amjad/OneDrive/المستندات/GWU_Cources/Spring2023/Machine_Learning/MLproject/Code")
 
 # Create an empty list to store the dataframes
-#df_list = []
+df_list = []
 
 # Loop through the files in the directory
-#for file in os.listdir():
+for file in os.listdir():
     # Check if the file is a CSV file
-    #if file.endswith(".csv"):
+    if file.endswith(".csv"):
         # Read the CSV file into a pandas dataframe
-       #df = pd.read_csv(file)
+       df = pd.read_csv(file)
         # Append the dataframe to the list
-        #df_list.append(df)
+       df_list.append(df)
 
 # Concatenate the dataframes in the list
-# concatenated_df = pd.concat(df_list, axis=0)
+concatenated_df = pd.concat(df_list, axis=0)
 
 # save it as one file
-# concatenated_df.to_csv("sounds.csv", index=False)
+concatenated_df.to_csv("sounds.csv", index=False)
 
 
-# ================================== Simple EDA ====================================
+# =============================== Simple EDA ====================================
 # set seed
 np.random.seed(42)
 
 # load dataset and saperate X,y
 sounds = pd.read_csv("sounds.csv")
 
-#X = sounds.iloc[:,:-1]
-#y = sounds.iloc[:,-1:]
-
-# shuffle the data
-# X, y = shuffle(X, y, random_state=42)
-
-# Understand data such as NA valuse and do graphs and data type
-
 # data shapes
 print(f'whole dataset schema {sounds.shape}')
-#print(f'whole dataset schema {X.shape}')
-#print(f'whole dataset schema {y.shape}')
 
 # check Na values
 print(f'number of Na in the whole dataframe {sounds.isna().sum().sum()}')
 
 # data head
 print(sounds.head(3))
-#print(X.head(3))
-#print(y.head(3))
 
 # check features types
 print(f'{sounds.dtypes}')
-#print(f'{X.dtypes}')
-#print(f'{y.dtypes}')
 
 # get the frequency of each activity to check for balanced data
 activity_counts = sounds['Activity'].value_counts()
@@ -85,25 +69,13 @@ plt.show()
 # make a copy of the main dataset 
 sounds2 = sounds.copy()
 
-# change y into numarical
+# change y into numarical for the model
 print(f'Activities before numarically label them {sounds2.iloc[:,-1].unique()}')
 sounds2['Activity'] = sounds2['Activity'].replace({'STANDING': 1, 'SITTING': 2, 'LAYING': 3, 'WALKING': 4, 'WALKING_DOWNSTAIRS':5, 'WALKING_UPSTAIRS':6})
 print(f'Activities values after labeling {sounds2.iloc[:,-1].unique()}')
 
 
-# =============================== plotting 3D ========================================
-
-# it needs working on like mayby using the raw daatset
-ax = plt.axes(projection='3d')
-ax.plot_surface(sounds2['tBodyAcc-mean()-X'],
-                sounds2['tBodyAcc-mean()-Y'],
-                sounds2['tBodyAcc-mean()-Z'], 
-                rstride=1, cstride=1,
-                cmap='viridis', edgecolor='none')
-ax.set_title('surface')
-
-
-#=============================== Clustering data to detect outliers ==================================
+#=============================== Detect outliers ==================================
 # Fit DBSCAN to the data
 clustering = DBSCAN(eps=5, min_samples=2).fit(sounds2)
 labels = clustering.labels_
