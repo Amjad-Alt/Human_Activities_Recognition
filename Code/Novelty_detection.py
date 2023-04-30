c1a4cb77cb7f9f4181253d78b2e059434346ee08
Amjad Altuwayjiri <amjadibrahim1994@gmail.com>
2023-04-30 03:53:33 -0400

    Update Novelty_detection.py

--- Code/Novelty_detection.py
+++ Code/Novelty_detection.py
@@ -1,16 +1,43 @@
 
+
+#=========================== LOAD DATA =========================
+
+# set seed
+np.random.seed(42)
+
+# train and split from the original data without deleteing outliers from the clusters
+
+sounds2.head()
+X_2 = sounds2.drop('Activity', axis=1)
+y_2 = sounds2['Activity']
+
+# X,y shapes
+print(f'X schema {X_2.shape}')
+print(f'y schema {y_2.shape}')
+
+# X,y head
+print(X_2.head(3))
+print(y_2.head(3))
+
+# check features types
+print(f'{X_2.dtypes}')
+print(f'{y_2.dtypes}')
+
+# split dataset into training, testing sets 60-40
+X_onetrain, X_valtest, y_onetrain, y_valtest = train_test_split(X_2, y_2, test_size=0.4, random_state=42)
+
 # Train the model
-model = OneClassSVM(nu=0.1)
+model = OneClassSVM(nu=0.05)
 # nu hyperparameter controls proportion of data that is considered anomalous
 #0.1 means that 10% of the data is considered anomalous.
 
-model.fit(X_train)
+model.fit(X_onetrain)
 
 # Predict anomalies on the testing set
-predictions = model.predict(X_test)
+predictions = model.predict(X_valtest)
 
 # Convert the labels to binary (1: normal, -1: anomalous)
-y_test_binary = np.where(y_test == 'normal', 1, -1)
+y_test_binary = np.where(y_valtest == 'normal', 1, -1)
 predictions_binary = np.where(predictions == 1, 1, -1)
 
 # Compute confusion matrix
@@ -23,13 +50,20 @@ print('False positives:', fp)
 print('True negatives:', tn)
 print('False negatives:', fn)
 
+# Confusion matrix:
+# True positives: 0
+# False positives: 3685
+# True negatives: 435
+# False negatives: 0
+
 # Compute accuracy
 accuracy = (tp + tn) / (tp + fp + tn + fn)
 print('Accuracy:', accuracy)
+#Accuracy: 0.10558252427184465
 
 
 # Get the decision function output for test set
-y_score = model.decision_function(X_test)
+y_score = model.decision_function(X_valtest)
 # Compute the ROC curve and ROC area for each class
 fpr, tpr, _ = roc_curve(predictions_binary, y_score)
 roc_auc = auc(fpr, tpr)
